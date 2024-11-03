10c3d1cdfe6a777d9b2034453774823bc8fea14e
Xinran Xu <70766915+Charlotte1031@users.noreply.github.com>
2024-11-03 15:07:48 -0500

    Add processing

--- DataInput.R
+++ DataInput.R
@@ -1,11 +1,33 @@
-data <- read.table('Data Tables/MTS1ClinicTesting.txt', 
-                   header = TRUE, sep = "|", fill = TRUE, stringsAsFactors = FALSE)
+
+AdvEvent <- read.table("Data Tables/MTS1AdvEvent.txt", 
+                       header = TRUE, sep = "|", fill = TRUE, stringsAsFactors = FALSE)
+
+head(AdvEvent) 
+
+
+VisitData <- read.table('Data Tables/MTS1ClinicTesting.txt', 
+                        header = TRUE, sep = "|", fill = TRUE, stringsAsFactors = FALSE)
 
 # View the first few rows
-head(data)
+head(VisitData)
 
-patient <- read.table("Data Tables/MTS1PtFinalStat.txt", 
-                      header = TRUE, sep = "|", fill = TRUE, stringsAsFactors = FALSE)
+PtRoster <- read.table("Data Tables/MTS1PtRoster.txt", 
+                       header = TRUE, sep = "|", fill = TRUE, stringsAsFactors = FALSE)
 
-head(patient) 
-  
+head(PtRoster) 
+
+
+# Y = SER for patient i at time j -> calculated by SE = S + C/2 -> MTS1ClinicTesting.txt,
+# S_OS = mean(AutoRef1SphOS, AutoRef2SphOS, AutoRef3SphOS)
+# C_OS = mean(AutoRef1CylOS, AutoRef2CylOS, AutoRef3CylOS)
+# 
+# S_OD = mean(AutoRef1SphOD, AutoRef2SphOD, AutoRef3SphOD)
+# C_OD = mean(AutoRef1CylOD, AutoRef2CylOD, AutoRef3CylOD)
+
+# time = timei -> 'Visit' -> MTS1ClinicTesting.txt (long format)
+# group = group/treatment assignment of time j -> MTS1PtRoster.txt 'TrtGroup'
+# Investigating variable: Parent status of myopia (is_mother_has_myopia -> 'MTS1DemoMedOcuHx.txt'$MotherMyop, is_father_has_myopia -> 'MTS1DemoMedOcuHx.txt'$FatherMyop) 
+# Adjusted variable: 
+# baseline SER, age (-> MTS1PtRoster.txt 'AgeAsOfEnrollDt'), iris color (brown or non-brown) (->'MTS1DemoMedOcuHx.txt' $ 'EyeColor'),  East Asian vs. non-East Asian race -> (->'MTS1DemoMedOcuHx.txt' $ 'EyeColor'),
+
+
---
+++ Processing.R
@@ -0,0 +1,43 @@
+# Load VisitData and PtRoster datasets
+VisitData <- read.table("Data Tables/MTS1ClinicTesting.txt", 
+                        header = TRUE, sep = "|", fill = TRUE, stringsAsFactors = FALSE)
+head(VisitData)
+
+PtRoster <- read.table("Data Tables/MTS1PtRoster.txt", 
+                       header = TRUE, sep = "|", fill = TRUE, stringsAsFactors = FALSE)
+head(PtRoster)
+
+# Load the additional dataset for parental myopia and other demographic information
+DemoMedOcuHx <- read.table("Data Tables/MTS1DemoMedOcuHx.txt", 
+                           header = TRUE, sep = "|", fill = TRUE, stringsAsFactors = FALSE)
+head(DemoMedOcuHx)
+
+# Step 1: Calculate SER for each eye
+# Left Eye (OS)
+VisitData$S_OS <- rowMeans(VisitData[, c("AutoRef1SphOS", "AutoRef2SphOS", "AutoRef3SphOS")], na.rm = TRUE)
+VisitData$C_OS <- rowMeans(VisitData[, c("AutoRef1CylOS", "AutoRef2CylOS", "AutoRef3CylOS")], na.rm = TRUE)
+VisitData$SER_OS <- VisitData$S_OS + (VisitData$C_OS / 2)
+
+# Right Eye (OD)
+VisitData$S_OD <- rowMeans(VisitData[, c("AutoRef1SphOD", "AutoRef2SphOD", "AutoRef3SphOD")], na.rm = TRUE)
+VisitData$C_OD <- rowMeans(VisitData[, c("AutoRef1CylOD", "AutoRef2CylOD", "AutoRef3CylOD")], na.rm = TRUE)
+VisitData$SER_OD <- VisitData$S_OD + (VisitData$C_OD / 2)
+
+# Step 2: Average SER across both eyes for each visit (i.e., time j)
+VisitData$SER <- rowMeans(VisitData[, c("SER_OS", "SER_OD")], na.rm = TRUE)
+
+# Step 3: Merge data with PtRoster and DemoMedOcuHx datasets
+# Merging VisitData with PtRoster on Patient ID
+MergedData <- merge(VisitData, PtRoster, by = "PtID")
+
+# Merging the result with DemoMedOcuHx on Patient ID
+MergedData <- merge(MergedData, DemoMedOcuHx, by = "PtID")
+
+# Step 4: Create necessary variables
+# Time (Visit) is already in VisitData as 'Visit'
+# Group/Treatment assignment
+MergedData$TreatmentGroup <- MergedData$TrtGroup
+
+# Parental myopia status (investigating variables)
+MergedData$is_mother_has_myopia <- MergedData$MotherMyop
+MergedData$is_father_has_myopia <- MergedData$Father
